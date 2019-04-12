05c344f4e33d5dd37428d621ddfe9f9d1a8f5e0d
Zach Overhulser <zoverhulser@gmail.com>
2019-04-12 15:56:16 +0000

    Mounts smurf data.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -8,9 +8,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+	componentDidMount() {
+		axios.get("https://lambda-school-1-zoverlvx.c9users.io:8080/smurfs")
+		.then(res => this.setState({smurfs: res.data}))
+		.catch((err) => console.log(err))
+	}
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
